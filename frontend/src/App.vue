0e4806bed4edec55991d8c4a7bebc6ce6268d0fa
NohEuijin <141835418+NohEuijin@users.noreply.github.com>
2024-03-11 17:27:14 +0900

    03/11 banner
    롯데시네마 배너 완료

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,54 +1,39 @@
 <template>
-  <router-view />
+  <body>
+    <!-- 배너 -->
+    <Banner/>
+    
+  </body>
 </template>
 
-<script setup>
-import { computed, reactive } from 'vue'
-import { useHead } from '@vueuse/head'
-
-// env 설정 적용 테스트용 → 프로젝트 시작시, 콘솔 확인 후 제거 필요
-console.log(import.meta.env, 'test here ')
-console.log(process.env, 'test here 22 ')
-
-const siteData = reactive({
-  title: `Vue3_project_test`,
-  description: `PROTECT Pick-here-project / Reserve-lite-project with Vue3`
-})
-useHead({
-  // Can be static or computed
-  title: computed(() => siteData.title),
-  meta: [
-    {
-      name: `description`,
-      content: computed(() => siteData.description)
-    }
-  ]
-})
-</script>
 <script>
-// import axios from 'axios'
+import { ref } from 'vue';
+import Banner from './components/Banner.vue';
 
 export default {
-  created() {
-    this.testMethod()
+  components : {
+    Banner
   },
-  methods: {
-    async testMethod() {
-      // await this.$store
-      //   .dispatch('testQuery')
-      //   .then((res) => console.log('graphql strapi_v4 test success !', res))
-      // await this.$store
-      //   .dispatch('terms')
-      //   .then((res) => console.log('graphql strapi_v3 test success !', res))
-      // await axios({
-      //   url: process.env.VUE_APP_BACKEND_URL + '/api/test-contents/1',
-      //   method: 'GET'
-      // }).then((res) => console.log('axios test success !!', res))
-    }
+  setup(){
+    
   }
 }
 </script>
 
 <style lang="scss">
 @import '@/assets/css/global.scss';
+//공통 css
+*{
+  margin: 0;
+  padding : 0;
+  list-style: none;
+  text-decoration: none;
+}
+body{
+  font-size: 12px;
+  color: #000;
+  min-width: 1024px;
+  font-family: "Noto Sans KR", "Roboto", "dotum", "sans-serif";
+}
+
 </style>
