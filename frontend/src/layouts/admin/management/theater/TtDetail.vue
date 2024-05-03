5df221377db2dddbc780a19a1ba60f5066b19e82
NohEuijin <141835418+NohEuijin@users.noreply.github.com>
2024-05-03 16:35:00 +0900

    영화 상세 페이지
    좌석 배치도
    - 규모
    - 복도
    - 빈공간
    - 좌석
    완료
    진행해야 될 내용
    -> 배치도에서 위 사항 적용하기!

--- frontend/src/layouts/admin/management/theater/TtDetail.vue
+++ frontend/src/layouts/admin/management/theater/TtDetail.vue
@@ -39,8 +39,8 @@
     <tbody>
       <tr>
         <td class="bg-gray100">번호</td>
-        <td>
-          <span>
+        <td class="ttd_table_width">
+          <span >
              {{ theater.id }}
           </span>
         </td>
@@ -58,12 +58,14 @@
         <td class="d-flex ttd_ratio_box">
           <input
           v-model="horizontalSeat"
-
+          @input="checkingNumber()"
           type="text" name="" id="">
           <span class="mt-1">x</span>
           <input
           v-model="verticalSeat"
+          @input="checkingNumber()"
           type="text" name="" id="">
+
         </td>
       </tr>
       <tr>
@@ -72,26 +74,52 @@
           <span>{{ theater.title }}</span>
         </td>
         <td class="bg-gray100">복도</td>
-        <td class="d-flex ttd_bokdo_box">
+        <td class="ttd_bokdo_box">
+          <span class="mt-0 mr-2 ttd_bokdo_total">
+            갯수(
+              <input
+              v-model="bokdoTotal"
+              @input="modifyBokdoTotal()"
+              type="text">
+              ) =
+          </span>
           <span v-for="(bokdoItem, bIndex) of bokdoList" :key="bIndex">
-          <input v-model="bokdoList[bIndex]" type="text">
+
+          <input
+          v-model="bokdoList[bIndex]"
+          @input="checkingNumber()"
+          type="text">
         </span>
         </td>
       </tr>
       <tr>
-        <td class="bg-gray100"></td>
+        <td class="bg-gray100">좌석</td>
         <td>
           <span>{{  }}</span>
         </td>
-        <td class="bg-gray100">빈공간</td>
-        <td  class="d-flex ttd_blank_box">
+        <td class="bg-gray100" >빈공간</td>
+        <td class="ttd_blank_box" colspan="2">
           <span v-for="(blankItem, bIndex) of blankList" :key="bIndex">
-            <input v-model="blankList[bIndex]" type="text" name="" id="">
+            <input
+            v-model="blankList[bIndex]"
+            @input="checkingNumber()"
+            type="text" name="" id="">
           </span>
         </td>
       </tr>
     </tbody>
   </v-table>
+  <v-col class="pb-0">
+    <span>
+      * 작은 화면에서는 배치도가 깨질수 있으니 큰 화면에서 이용해 주세요
+    </span>
+  </v-col>
+  <v-col class="pt-1">
+    <span>
+      * 빈 공간은 알파벳 대문자와 숫자 30 까지만 입력 해주세요
+    </span>
+  </v-col>
+
 
   <v-container class="d-flex pa-0 mt-10">
   <v-col cols="12">
@@ -103,15 +131,16 @@
       <v-col
         v-for="(vertical, vIndex) of verticalSeat"
         :key="vIndex"
-        class="pa-0 ma-0 verticalSeat"
+        class="pa-0 ma-0"
       >
   <v-row class="pa-0 ma-0">
     <v-col
       v-for="(horizontal, hIndex) of horizontalSeat"
       :key="hIndex"
       class="pa-1 ma-1 seatbox"
+      @click="getSelectedSeatValue(vIndex, hIndex)"
     >
-      {{ String.fromCharCode(65 + vIndex) }}{{ hIndex + 1 }}
+      {{ generatedSeatValue(vIndex, hIndex) }}
     </v-col>
   </v-row>
 </v-col>
@@ -134,8 +163,13 @@ export default {
       freeboardList:[],
       horizontalSeat:'',
       verticalSeat:'',
+      newHorizontalSeat:'',
+      newVerticalSeat:'',
       bokdoList:[],
       blankList:[],
+      seatValue:[],
+      bokdoTotal:'',
+      blankListValue:'',
     };
   },
   methods: {
@@ -150,11 +184,13 @@ export default {
         this.theater = res.theaters[0]
         this.horizontalSeat = Number(this.theater.ratio[0])
         this.verticalSeat = Number(this.theater.ratio[1])
-      
+
         for(let bd = 0; bd< res.theaters.length; bd++){
           res.theaters[bd].bokdo = res.theaters[bd].bokdo.split(',')
+
         }
         this.bokdoList = this.theater.bokdo
+        this.bokdoTotal = this.bokdoList.length
 
         for(let bk = 0; bk< res.theaters.length; bk++){
           res.theaters[bk].blank = res.theaters[bk].blank.split(',')
@@ -191,7 +227,88 @@ export default {
   async resetBtn(){
     await this.gettheaterDetail();
     // console.log('hello')
+  },
+  //(숫자 + 범위) 체크
+  checkingNumber() {
+    const numberCheck = /^[0-9]*$/;
+    if (!numberCheck.test(this.horizontalSeat)) {
+      this.horizontalSeat = '';
+    }else {
+      this.horizontalSeat = parseInt(this.horizontalSeat);
+      if (this.horizontalSeat > 30) {
+        this.horizontalSeat = 30;
+      }
+    }
+    if (!numberCheck.test(this.verticalSeat)) {
+      this.verticalSeat = '';
+    }else {
+      this.verticalSeat = parseInt(this.verticalSeat);
+      if (this.verticalSeat > 26) {
+        this.verticalSeat = 26;
+      }
+    }
+    for (let i = 0; i < this.bokdoList.length; i++) {
+    if (!numberCheck.test(this.bokdoList[i])) {
+      this.bokdoList[i] = '';
+    }else {
+      this.bokdoList[i] = parseInt(this.bokdoList[i]);
+      if (this.bokdoList[i] > 30) {
+        this.bokdoList[i] = 30;
+      }
+    }
   }
+  for (let i = 0; i < this.blankList.length; i++) {
+    const value = this.blankList[i];
+    if (!/^(?:[A-Z]|[A-Z][1-9][0-9]?)$/.test(value)) {
+        this.blankList[i] = '';
+    } else {
+        const letter = value.charAt(0);
+        let numbers = value.substring(1);
+        if (!numbers) {
+            numbers = '';
+        } else {
+            numbers = Math.min(parseInt(numbers), 30).toString();
+        }
+        this.blankList[i] = letter + numbers;
+    }
+  }
+    },
+    //좌석 값()
+    generatedSeatValue(vIndex, hIndex) {
+      return String.fromCharCode(65 + vIndex) + (hIndex + 1);
+    },
+    //좌석 값() 가지고 오기 -> 빈 공간에 추가
+    getSelectedSeatValue(vIndex, hIndex) {
+      this.seatValue = this.generatedSeatValue(vIndex, hIndex);
+      //좌석 배열에 들어가는 값 확인
+      for(let i=0;i<this.blankList.length;i++){
+        this.blankList[i]
+        this.blankListValue = this.blankList[i]
+      }
+      //같은 값 리턴, 다른 값 추가
+      if(this.seatValue===this.blankListValue){
+        return
+      }else{
+        this.blankList.push(this.seatValue)
+      }
+
+    },
+    modifyBokdoTotal(){
+      const numberCheck = /^[0-9]*$/;
+      for (let i = 0; i < this.bokdoTotal.length; i++) {
+    if (!numberCheck.test(this.bokdoTotal)) {
+      this.bokdoTotal = '';
+    }else {
+      this.bokdoTotal = parseInt(this.bokdoTotal);
+      if (this.bokdoTotal > 5) {
+        this.bokdoTotal = 5;
+      }
+    }
+  }
+      this.bokdoList.length = this.bokdoTotal
+        console.log(this.bokdoList.length)
+        console.log(this.bokdoTotal)
+    }
   },
   computed:{
     //페이지 카운터
@@ -215,6 +332,7 @@ export default {
 }
 .ms_dt_table td{
   border: 1px solid rgb(226, 226, 226) !important;
+  height: auto;
 }
 .ms_dt_table input{
   width: 100%;
@@ -258,62 +376,6 @@ export default {
   align-items: center !important;
   display: grid;
 }
-table{
-  // width: 100%;
-}
-// 게시글 테이블
-.msd_th_list > th:nth-child(n){
-  text-align: center !important;
-  background-color: rgb(240, 240, 240) !important;
-  font-weight: 600 !important;
-  border: 1px solid #f5f5f5 !important;
-  height: 30px;
-  font-size: 13px;
-}
-.msd_th_list > th:nth-child(1){
-  width: 300px;
-}
-.msd_th_list > th:nth-child(2){
-  width: 100px;
-}
-.msd_th_list > th:nth-child(3){
-  width: 70px;
-}
-.msd_th_list > th:nth-child(4){
-  width: 70px;
-}
-.msd_td_list > td:nth-child(n){
-  text-align: center !important;
-  border: 1px solid #f5f5f5 !important;
-  height: 30px;
-  // font-size: 13px;
-}
-.msd_th_list2 > th:nth-child(n){
-  text-align: center !important;
-  background-color: rgb(240, 240, 240) !important;
-  font-weight: 600 !important;
-  border: 1px solid #f5f5f5 !important;
-  height: 30px;
-  font-size: 13px;
-}
-.msd_th_list2 > th:nth-child(1){
-  width: 250px;
-}
-.msd_th_list2 > th:nth-child(2){
-  width: 100px;
-}
-.msd_th_list2 > th:nth-child(3){
-  width: 70px;
-}
-.msd_th_list2 > th:nth-child(4){
-  width: 100px;
-}
-.msd_td_list2 > td:nth-child(n){
-  text-align: center !important;
-  border: 1px solid #f5f5f5 !important;
-  height: 30px;
-  // font-size: 13px;
-}
 .screen{
   width: 100%;
   height: 10%;
@@ -332,6 +394,7 @@ table{
   border-top-right-radius: 4px;
   // text-align: center;
   // background-color: black;
+  cursor: pointer;
 }
 .ttd_ratio_box input{
   margin-top: 2px;
@@ -342,10 +405,11 @@ table{
 }
 .ttd_blank_box{
   min-width: 100px;
+  height: auto;
 }
 .ttd_blank_box input{
   margin-top: 2px;
-  width: 45px;
+  width: 48px;
   font-weight: 600 !important;
   border: 1px solid #f5f5f5;
   text-align: center
@@ -363,4 +427,11 @@ table{
   // justify-content: center !important;
   // align-items: center !important;
 }
+.ttd_table_width{
+  min-width: 200px;
+}
+// .ttd_bokdo_total{
+//   font-size: 13px;
+//   font-weight: 500;
+// }
 </style>
